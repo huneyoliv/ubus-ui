d03719615f96fe193c56acd8e32bee1456a5a3db
Huney Oliveira <huney.oliveira@gmail.com>
2026-06-10 17:46:10 -0300

    build: update versionName to 1.1.0 to match SemVer guidelines

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -85,7 +85,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 2
-        versionName = "1.1-ui-fixes"
+        versionName = "1.1.0"
     }
     packaging {
         resources {
